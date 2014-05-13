d1abb93519e9a34de921efe46772eae6277da5e1
Ethan Taubman <etaubman@gmail.com>
2014-05-12 22:54:54 -0400

    Preliminary Layout

---
+++ js/jway.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors: ['jway', 'how','benefits','product','contact'],
+		slidesColor: ['rgb(43,182,216)', 'white','rgb(249,154,11)','white','rgb(43,182,216)'],
+		css3: true,
+		autoScrolling:true,
+		touchSensitivity:10
+	});
+});
