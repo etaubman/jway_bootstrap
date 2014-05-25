0719bb02ebb4f3d0c77926b51f3757d86093bbf5
Ethan Taubman <etaubman@gmail.com>
2014-05-25 19:27:20 -0400

    added some content from original site

--- js/jway.js
+++ js/jway.js
@@ -1,9 +1,33 @@
+ var jway = {
+ 	"colors" : {
+ 		"blue" : "#2BB6FF",
+ 		"orange" : "#F99A0B",
+ 		"white" : "#FFFFFF",
+ 	}
+ };
+
+
  $(document).ready(function() {
+
+ 	//Initialize fullpage plugin
 	$('#fullpage').fullpage({
 		anchors: ['jway', 'how','benefits','product','contact'],
-		slidesColor: ['rgb(43,182,216)', 'white','rgb(249,154,11)','white','rgb(43,182,216)'],
+		slidesColor: [
+						jway.colors.blue, 
+						jway.colors.white, 
+						jway.colors.orange,
+						jway.colors.white,
+						jway.colors.blue
+					],
 		css3: true,
 		autoScrolling:true,
 		touchSensitivity:10
 	});
+
+
+	//Setup our links
+	$(window).hashchange(function(){
+
+	});
+
 });
